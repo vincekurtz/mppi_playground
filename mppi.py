9c4b43d5e3ee0663c0fda58ba5e6b3c055c7db7f
Vince Kurtz <vjkurtz@gmail.com>
2023-11-14 20:20:48 -0800

    badly tuned MPPI working

--- mppi.py
+++ mppi.py
@@ -3,6 +3,7 @@
 import pygame
 import numpy as np
 import time
+from typing import List
 
 TIME_STEP = 0.01
 TEMPERATURE = 1.0
@@ -16,14 +17,6 @@ def robot_dynamics(x: np.array, u: np.array) -> np.array:
     robot with integrator dynamics. 
     """
     return x + u * TIME_STEP
-
-def proportional_controller(x: np.array, x_nom: np.array) -> np.array:
-    """
-    Given the state x and the target state x_nom, return a control input
-    u that will drive the robot towards the target state.
-    """
-    k = 2.0
-    return -k * (x - x_nom)
 
 def sample_control_tape(x0: np.array, u_nom: np.array) -> np.array:
     """
@@ -46,6 +39,61 @@ def rollout(x0: np.array, u_tape: np.array) -> np.array:
         x_traj.append(x)
     return np.array(x_traj)
 
+def compute_cost(x: np.array, x_nom: np.array, u: np.array) -> float:
+    """
+    Given the state x, the nominal state x_nom, and the control u, return
+    the running cost. 
+    """
+    return np.linalg.norm(x - x_nom)**2 + 0.01 * np.linalg.norm(u)**2
+
+def compute_trajectory_cost(x_traj: np.array, x_nom: np.array, u_tape: np.array) -> float:
+    """
+    Given the state trajectory x_traj, the nominal state x_nom, and the
+    control tape u_tape, return the total cost.
+    """
+    cost = 0.0
+    for x, u in zip(x_traj, u_tape):
+        cost += compute_cost(x, x_nom, u)
+    return cost
+
+def vanilla_mppi(x0: np.array, x_nom: np.array, u_guess: np.array) -> (List[np.array], List[np.array]):
+    """
+    Given the initial state x0, the nominal state x_nom, and an initial guess
+    for the control tape, perform MPPI to get a new control tape.
+
+    Returns a list of control tapes and a list of state trajectories, where the
+    last element of each list is the best control tape and state trajectory.
+    """
+    # Sample some trajectories
+    Us = []
+    Xs = []
+    costs = []
+    for _ in range(NUM_SAMPLES):
+        u_tape = sample_control_tape(x0, u_guess)
+        x_tape = rollout(x0, u_tape)
+        Us.append(u_tape)
+        Xs.append(x_tape)
+        costs.append(compute_trajectory_cost(x_tape, x_nom, u_tape))
+
+    # Compute the weights
+    costs = np.array(costs)
+    min_cost = np.min(costs)
+    weights = np.exp(-(costs-min_cost) / TEMPERATURE)
+    weights /= np.sum(weights)
+
+    # Compute the new control tape
+    u_nom = np.zeros(u_guess.shape)
+    for u_tape, weight in zip(Us, weights):
+        u_nom += weight * u_tape
+
+    # Compute the new state trajectory
+    x_nom = rollout(x0, u_nom)
+
+    # Append the new control tape and state trajectory
+    Us.append(u_nom)
+    Xs.append(x_nom)
+
+    return Us, Xs
 
 
 # Initialize pygame
@@ -63,8 +111,6 @@ x_nom = np.array([400, 250])
 
 # Initialize the nominal control tape
 u_nom = np.array([[500, 0.0] for _ in range(HORIZON)])
-x_traj = rollout(x, u_nom)
-print(x_traj)
 
 # Run until the user asks to quit
 running = True
@@ -80,18 +126,23 @@ while running:
     pygame.draw.circle(screen, (0, 255, 0), x_nom, 10)
 
     # Compute the control input
-    u = proportional_controller(x, x_nom)
+    u = u_nom[0]
 
-    # Sample some control tapes
-    for _ in range(NUM_SAMPLES):
-        u_tape = sample_control_tape(x, u_nom)
-        x_traj = rollout(x, u_tape)
+    # Perform an MPPI step
+    Us, Xs = vanilla_mppi(x, x_nom, u_nom)
 
-        for t in range(HORIZON-1):
-            pygame.draw.line(screen, (255, 0, 0), x_traj[t], x_traj[t+1])
+    # Visualize the MPPI samples
+    for x_traj in Xs:
+        for t in range(len(x_traj)-1):
+            pygame.draw.line(screen, (255, 0, 0), x_traj[t, :], x_traj[t+1,:], width=1)
+
+    # Visualize the best trajectory with a thicker line
+    x_star = Xs[-1]
+    for t in range(len(x_star)-1):
+        pygame.draw.line(screen, (15, 20, 20), x_star[t, :], x_star[t+1,:], width=3)
 
     # Update the nominal control tape
-    u_nom = np.array([u for _ in range(HORIZON)])
+    u_nom = Us[-1]
 
     # Update the state
     x = robot_dynamics(x, u)
